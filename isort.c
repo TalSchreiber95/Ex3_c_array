57af89d5deb718f569aec9dc2ffbee76afd591ad
Tal Schreiber <58589007+TalSchreiber95@users.noreply.github.com>
2021-01-07 16:49:06 +0200

    Update isort.c

--- isort.c
+++ isort.c
@@ -1,6 +1,7 @@
 #include "isort.h"
 #include <stdio.h>
-
+// {1,3,-2,4} arr point to 1 and i=2 should return
+// {1,3,3,-2}
 void shift_element(int* arr, int i) {
     int temp1 = *(arr);
     for (int j = 1; j <= i; j++) {
@@ -16,12 +17,11 @@ void insertion_sort(int* arr , int len) {
         int countDown = i;
         while (countDown > 0 && currentNumber < *(arr + countDown - 1)) {
             count++;
-            countDown = countDown - 1;
+            countDown--;
         }
         if (count > 0) {
             shift_element((arr+i - count), count);
             *(arr+i - count) = currentNumber;
         }
-
     }
 }
